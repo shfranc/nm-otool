d972a28ec6e5968b4dadafe3832d1afce4997de5
Sarah Franc <shfranc@gmail.com>
2018-09-18 18:41:39 +0200

    ADD: options.c to get and save options

--- nm/incs/nm.h
+++ nm/incs/nm.h
@@ -6,17 +6,27 @@
 /*   By: sfranc <sfranc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/18 15:44:27 by sfranc            #+#    #+#             */
-/*   Updated: 2018/09/18 16:58:13 by sfranc           ###   ########.fr       */
+/*   Updated: 2018/09/18 18:24:47 by sfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef NM_H
 # define NM_H
 
-# define DEFAULT	"a.out"
+# define DEFAULT		"a.out"
+# define OPTIONS		"agnoprumxjlfPA"
+# define USAGE			"usage: ./ft_nm [ -agnoprumxjlfPA ] [ - ] [ file ... ]"
+# define ILLEGAL_OPTION	"ft_nm: illegal option\n" USAGE
 
 # include "libft.h"
 
+extern int	g_flags;
+
 void	ft_nm(char *filename);
+
+/*
+** PARSING
+*/
+int		ft_get_options(int *argc, char ***argv);
 
 #endif
--- nm/srcs/main.c
+++ nm/srcs/main.c
@@ -6,19 +6,27 @@
 /*   By: sfranc <sfranc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/18 15:39:40 by sfranc            #+#    #+#             */
-/*   Updated: 2018/09/18 16:59:02 by sfranc           ###   ########.fr       */
+/*   Updated: 2018/09/18 18:26:42 by sfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "nm.h"
 
+int 	g_flags = 0;
+
 int		main(int ac, char **av)
 {
-	(void)av;
-
 	if (ac == 1)
 		ft_nm(DEFAULT);
 	else
-		ft_nm(*(++av));	
+	{
+		if ((ft_get_options(&ac, &av)) == -1)
+		{
+			ft_putendl_fd(ILLEGAL_OPTION, STDERR_FILENO);
+			return (EXIT_FAILURE);
+		}
+		while (ac--)
+			ft_nm(*(av++));
+	}
 	return (EXIT_SUCCESS);
 }
