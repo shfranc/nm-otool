6267653c45bdfdc861d133c17fada3cfce039b8e
Sarah Franc <shfranc@gmail.com>
2018-09-26 11:05:52 +0200

    UPD: archive wip

--- nm/srcs/archive.c
+++ nm/srcs/archive.c
@@ -6,24 +6,37 @@
 /*   By: sfranc <sfranc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/24 16:52:12 by sfranc            #+#    #+#             */
-/*   Updated: 2018/09/25 18:16:52 by sfranc           ###   ########.fr       */
+/*   Updated: 2018/09/26 10:49:04 by sfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "nm.h"
 #include <stdio.h>
 
+static int     ft_padding_ar_mac(char *ar_name)
+{
+    int     ar_name_size;
+
+    if ((ft_strncmp(ar_name, AR_EFMT1, ft_strlen(AR_EFMT1))) == 0)
+    {
+        ar_name_size = ft_atoi(ar_name + ft_strlen(AR_EFMT1));
+        printf("ar name size: %d\n", ar_name_size);
+    }
+    else
+        ar_name_size = 0;
+    return (ar_name_size);
+}
 
 
 int     ft_handle_archive(void *ptr)
 {
     struct ar_hdr       *header;
     char                *long_name;
-    unsigned int        long_name_size;    
     unsigned int        symtab_size;
+    unsigned int        nb_items;    
     struct ranlib       *obj;
 
-    header = (struct ar_hdr)(ptr + SARMAG);
+    header = (struct ar_hdr*)(ptr + SARMAG);
     printf("sizeof ar_hdr: %lu\n", sizeof(struct ar_hdr));
     
     write(1, "\n", 1);
@@ -44,16 +57,16 @@ int     ft_handle_archive(void *ptr)
     long_name = (char*)(header + 1);
     printf("longname: %s\n", long_name);
 
-    long_name_size = *(unsigned int*)(long_name + 1);
-    printf("longname size: %d\n", long_name_size);
-    
-    symtab_size = *(int*)((void*)(header + 1) + sizeof(long_name));
+    symtab_size = *(unsigned int*)((void*)(header + 1) + ft_padding_ar_mac(header->ar_name));
     printf("size of symble table: %d\n", symtab_size);
 
-    obj = (struct ranlib*) ((void*)(header + 1) + 24);
-    // printf("ran_strx: %d\n", obj->ran_un);
-    // printf("ran_name: %u\n", obj->ran_un);
+    nb_items = symtab_size / sizeof(struct ranlib);
+    printf("nb items: %d\n", nb_items);    
+
+    obj = (struct ranlib*) ((void*)(header + 1) + ft_padding_ar_mac(header->ar_name) + sizeof(unsigned int));
+    printf("ran_strx: %d\n", obj->ran_un.ran_strx);
     printf("ran_off: %u\n", obj->ran_off);
+    printf("object name: %s\n", ptr + obj->ran_un.ran_strx);
     
     return (0);
 }
