04fdab5e4d79877e5fa4306a962d31a9c537a3c1
Sarah Franc <shfranc@gmail.com>
2019-06-07 00:27:54 +0200

    refacto(archive): norme

--- nm/srcs/archive.c
+++ nm/srcs/archive.c
@@ -8,7 +8,8 @@ static uint32_t     ft_padding_ar_mac(char *ar_name)
         return (0);
 }
 
-t_ex_ret	loop_through_archive(t_bin_file *file, struct ar_hdr *object_header)
+static t_ex_ret	loop_through_archive(t_bin_file *file,
+					struct ar_hdr *object_header)
 {
 	char				*object_name;
 	void				*object_ptr;
@@ -19,8 +20,11 @@ t_ex_ret	loop_through_archive(t_bin_file *file, struct ar_hdr *object_header)
 		object_name = (char*)(object_header + 1);
 		object_ptr = object_name + ft_padding_ar_mac(object_header->ar_name);
 		object_size = ft_atoi(object_header->ar_size);
-		if (!is_in_file(file, object_ptr, object_size - ft_padding_ar_mac(object_header->ar_name)))
+		if (!is_in_file(file, object_ptr, object_size \
+			- ft_padding_ar_mac(object_header->ar_name)))
+		{
 			return (put_error(object_name, TRUNC_OBJECT));
+		}
 		if (ft_nm(file->filename, object_name, object_size, object_ptr))
 			return (FAILURE);
 		object_header = (struct ar_hdr*)is_in_file(file, object_name \
@@ -29,27 +33,58 @@ t_ex_ret	loop_through_archive(t_bin_file *file, struct ar_hdr *object_header)
 	return (SUCCESS);
 }
 
+static t_ex_ret	check_valid_archive(t_bin_file *file, struct ar_hdr *header,
+				uint32_t *symtab_size, uint32_t *string_table_size)
+{
+    struct ranlib       *symtab;
+
+    *symtab_size = *(uint32_t*)is_in_file(file, ((void*)(header + 1) \
+		+ ft_padding_ar_mac(header->ar_name)), sizeof(uint32_t));
+	if (!symtab_size)
+		return (put_error(file->filename, TRUNC_OBJECT));
+    symtab = (struct ranlib*)is_in_file(file, ((void*)(header + 1) \
+		+ ft_padding_ar_mac(header->ar_name) + sizeof(uint32_t)), *symtab_size);
+	if (!symtab)
+		return (put_error(file->filename, TRUNC_OBJECT));
+	*string_table_size = *(uint32_t*)is_in_file(file, ((void*)symtab \
+		+ *symtab_size), sizeof(uint32_t));
+	if (!string_table_size)
+		return (put_error(file->filename, TRUNC_OBJECT));
+	return (SUCCESS);
+}
+
+static void		init_file(t_bin_file *file, char *filename, size_t size,
+					void *ptr)
+{
+	file->ptr = ptr;
+	file->end = ptr + size;
+	file->filename = filename;
+}
+
 t_ex_ret     handle_archive(char *filename, size_t size, void *ptr)
 {
-    struct ar_hdr       *header;
-    unsigned int        symtab_size;
-    struct ranlib       *symtab;
-	unsigned int		string_table_size;
-    struct ar_hdr       *object_header;
 	t_bin_file			file;
+    struct ar_hdr       *header;
+    uint32_t        	symtab_size;
+	uint32_t			string_table_size;
+    struct ar_hdr       *object_header;
 
-	file.ptr = ptr;
-	file.end = ptr + size;
-	file.filename = filename;
-    header = (struct ar_hdr*)(ptr + SARMAG);
-    symtab_size = *(unsigned int*)((void*)(header + 1) \
-		+ ft_padding_ar_mac(header->ar_name));
-    symtab = (struct ranlib*) ((void*)(header + 1) \
-		+ ft_padding_ar_mac(header->ar_name) + sizeof(unsigned int));
-	string_table_size = *(unsigned int*)((void*)symtab + symtab_size);
+	init_file(&file, filename, size, ptr);
+	symtab_size = 0;
+	string_table_size = 0;
+    header = (struct ar_hdr*)is_in_file(&file, ptr + SARMAG, sizeof(*header));
+	if (!header)
+		return (put_error(filename, VALID_OBJECT));
+	if (check_valid_archive(&file, header, &symtab_size, &string_table_size) \
+		== FAILURE)
+	{
+		return (FAILURE);
+	}
     object_header = (struct ar_hdr*)is_in_file(&file, (void*)(header + 1) \
-		+ ft_padding_ar_mac(header->ar_name) + sizeof(unsigned int) \
+		+ ft_padding_ar_mac(header->ar_name) + sizeof(uint32_t) \
 		+ symtab_size + sizeof(uint32_t) \
 		+ string_table_size, sizeof(*object_header));
+	if (!object_header)
+		return (put_error(filename, TRUNC_OBJECT));
     return (loop_through_archive(&file, object_header));
 }
