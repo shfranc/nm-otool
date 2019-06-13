220f37875ace1dd6385a34ebe881165632f93cd6
Sarah Franc <sfranc@student.42.fr>
2019-06-13 12:16:52 +0200

    refacto(norme): ft_nm

--- nm/srcs/ft_nm.c
+++ nm/srcs/ft_nm.c
@@ -23,31 +23,27 @@ static void		put_filename(char *filename, char *archive_name)
 t_ex_ret		ft_nm(char *archive_name, char *filename, uint64_t size,
 					void *ptr)
 {
-	t_ex_ret		ret;
 	uint32_t		magic_number;
 
 	put_filename(filename, archive_name);
 	magic_number = *(uint32_t *)ptr;
-	ret = FAILURE;
 	if (magic_number == MH_MAGIC)
-		ret = handle_32(MAGIC, filename, size, ptr);
-	else if (magic_number == MH_MAGIC_64)
-		ret = handle_64(MAGIC, filename, size, ptr);
-	else if (magic_number == MH_CIGAM)
-		ret = handle_32(CIGAM, filename, size, ptr);
-	else if (magic_number == MH_CIGAM_64)
-		ret = handle_64(CIGAM, filename, size, ptr);
-	else if (ft_strncmp((char *)ptr, ARMAG, SARMAG) == 0)
-		ret = handle_archive(filename, size, ptr);
-	else if (magic_number == FAT_MAGIC)
-		ret = handle_fat32(MAGIC, filename, size, ptr);
-	else if (magic_number == FAT_CIGAM)
-		ret = handle_fat32(CIGAM, filename, size, ptr);
-	else if (magic_number == FAT_MAGIC_64)
+		return (handle_32(MAGIC, filename, size, ptr));
+	if (magic_number == MH_MAGIC_64)
+		return (handle_64(MAGIC, filename, size, ptr));
+	if (magic_number == MH_CIGAM)
+		return (handle_32(CIGAM, filename, size, ptr));
+	if (magic_number == MH_CIGAM_64)
+		return (handle_64(CIGAM, filename, size, ptr));
+	if (ft_strncmp((char *)ptr, ARMAG, SARMAG) == 0)
+		return (handle_archive(filename, size, ptr));
+	if (magic_number == FAT_MAGIC)
+		return (handle_fat32(MAGIC, filename, size, ptr));
+	if (magic_number == FAT_CIGAM)
+		return (handle_fat32(CIGAM, filename, size, ptr));
+	if (magic_number == FAT_MAGIC_64)
 		ft_putendl("FAT 64 MAGIC");
-	else if (magic_number == FAT_CIGAM_64)
+	if (magic_number == FAT_CIGAM_64)
 		ft_putendl("FAT 64 CIGAM");
-	else
-		return (put_error(filename, VALID_OBJECT));
-	return (ret);
+	return (put_error(filename, VALID_OBJECT));
 }
